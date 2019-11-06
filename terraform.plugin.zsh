45430cab5da869932dd4f8b8162eb7f7ac27fc0b
Thuan Duong <thuandt26@gmail.com>
2019-11-07 03:12:09 +0700

    Add terraform zsh completion

---
+++ terraform.plugin.zsh
@@ -0,0 +1,23 @@
+# According to the Zsh Plugin Standard:
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+#compdef terraform
+complete -o nospace -C $(asdf which terraform) terraform
+
+alias tf="terraform"
+
+# from OMZ::plugins/terraform
+function tf_prompt_info() {
+    # dont show 'default' workspace in home dir
+    [[ "$PWD" == ~ ]] && return
+    # check if in terraform dir
+    if [ -d .terraform ]; then
+      workspace=$(terraform workspace show 2> /dev/null) || return
+      echo "[${workspace}]"
+    fi
+}
+
+# Then ${0:h} to get plugin's directory
