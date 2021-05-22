caac7d25d125ecdc80b180ad506938f5e1fa6bd6
Thuan Duong <thuandt26@gmail.com>
2021-05-22 17:08:02 +0700

    Update tf_prompt_info and zinit headers

--- zsh-terraform.plugin.zsh
+++ zsh-terraform.plugin.zsh
@@ -1,8 +1,22 @@
+# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+
+# Copyright (c) 2021 Thuan Duong
+
 # According to the Zsh Plugin Standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+# Then ${0:h} to get plugin's directory
+
+if [[ ${zsh_loaded_plugins[-1]} != */zsh-terraform && -z ${fpath[(r)${0:h}]} ]] {
+    fpath+=( "${0:h}" )
+}
+
+# Standard hash for plugins, to not pollute the namespace
+typeset -gA Plugins
+Plugins[ZSH_TERRAFORM_DIR]="${0:h}"
 
 #compdef terraform
 complete -o nospace -C $(asdf which terraform) terraform
@@ -32,24 +46,17 @@ tfd(){
 }
 
 # from OMZ::plugins/terraform
-tf_prompt_info() {
+function tf_prompt_info() {
   # dont show 'default' workspace in home dir
   [[ "$PWD" == ~ ]] && return
   # check if in terraform dir
-  if [ -d .terraform ]; then
-    workspace=$(terraform workspace show 2> /dev/null) || return
+  if [[ -d .terraform && -r .terraform/environment  ]]; then
+    workspace=$(cat .terraform/environment) || return
     echo "[${workspace}]"
   fi
 }
 
-tg_prompt_info() {
-    # dont show 'default' workspace in home dir
-    [[ "$PWD" == ~ ]] && return
-    # check if in terraform dir
-    if [ -d .terraform ]; then
-      workspace=$(terrgrunt workspace show 2> /dev/null) || return
-      echo "[${workspace}]"
-    fi
-}
+# Use alternate vim marks [[[ and ]]] as the original ones can
+# confuse nested substitutions, e.g.: ${${${VAR}}}
 
-# Then ${0:h} to get plugin's directory
+# vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
